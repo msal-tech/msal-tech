9a952975277a4220e1a548fb869359659df70ab7
askras <askrasnikov@gmail.com>
2025-02-25 23:12:21 +0300

    Изменено: Основная версия проекта изменена на 3.10

--- tests/venv_test.py
+++ tests/venv_test.py
@@ -7,4 +7,4 @@ def test_python_version():
     """Checking the python version"""
     major, minor = platform.python_version().split(".")[:2]
     pyversion = f"{major}.{minor}"
-    assert float(pyversion) >= 3.12, "Incorrect python version"
+    assert float(pyversion) >= 3.10, "Incorrect python version"
